5ab0668ce4c109ca7c0665fe22a4f71b5675e353
kgw4it <30734393+kgw4it@users.noreply.github.com>
2018-04-18 23:42:44 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ cd `dirname $0`
 # Download Mattermost-LDAP
 git clone https://github.com/Crivaledaz/Mattermost-LDAP.git
 
+patch -p0 -d Mattermost-LDAP < Mattermost-LDAP.patch
+
 # Move files to Dockerfile folders.
 mv Mattermost-LDAP/oauth app/
 rm -rf Mattermost-LDAP
