9845656f31b44edb703d87eb0bd58d96f33fba80
kgw4it <30734393+kgw4it@users.noreply.github.com>
2018-04-18 22:32:21 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,16 +2,8 @@
 cd `dirname $0`
 
 # Download Mattermost-LDAP
-commit_id=502ae7ecb4ca9ffd93b558185f575f64407ad95a
-curl -OL https://github.com/Crivaledaz/Mattermost-LDAP/archive/${commit_id}.tar.gz
-tar zxvf ${commit_id}.tar.gz
-rm ${commit_id}.tar.gz
-mv Mattermost-LDAP-${commit_id} Mattermost-LDAP
-
-# Apply patch
-patch -p0 -d Mattermost-LDAP < Mattermost-LDAP.patch
+git clone https://github.com/Crivaledaz/Mattermost-LDAP.git
 
 # Move files to Dockerfile folders.
 mv Mattermost-LDAP/oauth app/
-mv Mattermost-LDAP/init_postgres.sh db/
 rm -rf Mattermost-LDAP
